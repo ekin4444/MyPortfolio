8fadca5ddd33d2105cb6167c769ca84e1c663ee8
ekinf <ekinfiliza@hotmail.com>
2024-04-07 11:02:31 +0200

    template added

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
+# urls.py
+
 from django.urls import path
 from . import views
 
-from .views import index, redirect_urls
-
 urlpatterns = [
-    path('', index, name='index'),
-    path('<slug>/', redirect_urls, name='redirect_urls'),
+    path('', views.index, name='index'),
+    # Add other URL patterns for blog and portfolio if needed
 ]
